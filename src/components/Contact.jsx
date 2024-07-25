1cc84f7ce4f205f1961f104c2d02e475d610ed20
user.name: "techaffairs4@gmail.com <techaffairs4@gmail.com>
2024-07-26 03:16:13 +0530

    UI is updated

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -35,7 +35,7 @@ const Contact = () => {
             Let us keep in touch
           </p>
         </div>
-        <div className="flex flex-col gap-2 md:flex-row w-full mx-auto rounded-xl bg-gray-200 text-black dark:bg-neutral-950 dark:text-white p-4 md:gap-7 lg:gap-9 lg:rounded-2xl xl:gap-10">
+        <div className="flex flex-col gap-2 md:flex-row w-full mx-auto rounded-xl bg-neutral-100 border border-blue-400 shadow-inner shadow-blue-400 text-black dark:bg-neutral-900 dark:text-white p-4 md:gap-7 lg:gap-9 lg:rounded-2xl xl:gap-10">
           <div className="p-2 w-full text-center lg:p-5 xl:p-7 md:w-1/2 lg:w-4/6">
             <h1  className="hidden md:block text-2xl lg:text-3xl text-green-400 mb-3 lg:mb-4">
               Get In Touch
@@ -49,7 +49,7 @@ const Contact = () => {
             </div>
             <div  className="flex gap-3 items-center mb-4 md:gap-2 lg:gap-5">
               <FaEnvelope className="dark:text-white text-black" />
-              <a href="mailto:talakolavijaykumar@gmail.com" className=" md:text-lg">techaffairs4@gmail.com</a>
+              <a href="mailto:techaffairs4@gmail.com" className=" md:text-lg">techaffairs4@gmail.com</a>
             </div>
             <div  className="flex gap-3 items-center md:gap-2 lg:gap-5">
               <FaMapMarkerAlt className="dark:text-white text-black" />
@@ -58,13 +58,13 @@ const Contact = () => {
           </div>
           <form  ref={formRef} onSubmit={handleSubmit} className="flex flex-col p-2 md:w-1/2 md:p-4 lg:px-5 lg:py-7 lg:m-4 lg:w-3/5">
             <div  className="p-2 w-full">
-              <input required placeholder="Name" type="text" name="from_name" className="mb-1 w-full bg-gray-400 placeholder-black dark:placeholder-white rounded-md border border-gray-100 focus:border-indigo-500 focus:bg-gray-200 focus:ring-2 focus:ring-indigo-600 tplaceholder-black dark:placeholder-whiteext-base outline-none text-black p-2 leading-8 transition-colors duration-200 ease-in-out"/>
+              <input required placeholder="Name" type="text" name="from_name" className="mb-1 w-full bg-gray-300 dark:bg-gray-700 placeholder-black dark:placeholder-white rounded-md border border-green-400 shadow-inner shadow-green-400 focus:border-indigo-500 focus:bg-gray-100 focus:ring-2 focus:ring-indigo-600 focus:border-none focus:shadow-none tplaceholder-black dark:placeholder-whiteext-base outline-none text-black dark:text-white p-2 leading-8 transition-colors duration-200 ease-in-out"/>
             </div>
             <div  className="p-2 w-full">
-              <input required placeholder="Email" type="email" name="from_email" className="mb-1 w-full bg-gray-400 placeholder-black dark:placeholder-white rounded-md border border-gray-100 focus:border-indigo-500 focus:bg-gray-200 focus:ring-2 focus:ring-indigo-600 text-base outline-none text-black p-2 leading-8 transition-colors duration-200 ease-in-out"/>
+              <input required placeholder="Email" type="email" name="from_email" className="mb-1 w-full bg-gray-300 dark:bg-gray-700 placeholder-black dark:placeholder-white rounded-md border border-green-400 shadow-inner shadow-green-400 focus:border-indigo-500 focus:bg-gray-100 focus:ring-2 focus:ring-indigo-600 focus:border-none focus:shadow-none text-base outline-none text-black dark:text-white p-2 leading-8 transition-colors duration-200 ease-in-out"/>
             </div>
             <div className="p-2 w-full">
-              <textarea required placeholder="Message" name="message" className="mb-1 w-full bg-gray-400 placeholder-black dark:placeholder-white rounded-md border border-gray-100 focus:border-indigo-500 focus:bg-gray-200 focus:ring-2 focus:ring-indigo-600 h-32 text-base outline-none text-black p-2 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+              <textarea required placeholder="Message" name="message" className="mb-1 w-full bg-gray-300 dark:bg-gray-700 placeholder-black dark:placeholder-white rounded-md border border-green-400 shadow-inner shadow-green-400 focus:border-indigo-500 focus:bg-gray-100 focus:ring-2 focus:ring-indigo-600 focus:border-none focus:shadow-none h-32 text-base outline-none text-black dark:text-white p-2 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
             </div>
             <div className="p-2 w-full">
               <button className=" font-medium mx-auto my-3 text-white bg-green-600 border-0 py-2 px-12 focus:outline-none hover:scale-110 hover:bg-green-300 hover:text-black transition duration-500 rounded-xl text-lg">
