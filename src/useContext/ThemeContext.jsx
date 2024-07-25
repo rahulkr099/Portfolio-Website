d5effbe6c859f58810182e068a7c5b76343f0d92
user.name: "techaffairs4@gmail.com <techaffairs4@gmail.com>
2024-07-25 16:00:29 +0530

    animations is fixed

--- src/useContext/ThemeContext.jsx
+++ src/useContext/ThemeContext.jsx
@@ -1,4 +1,6 @@
-import { createContext,useCallback,useContext, useEffect, useState } from "react";
+import { createContext,useContext, useEffect, useState } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const ThemeContext = createContext();
 
@@ -11,14 +13,10 @@ export const ThemeProvider = ({children}) =>{
     const [isDarkMode,setIsDarkMode]= useState(false);
 
     const toggleTheme = ()=>{
+        AOS.refresh();
         setIsDarkMode((prevState)=>!prevState);
     };
     const theme = isDarkMode?'Dark':'Light';
-    
-
-    
-  
-
     useEffect(()=>{
         toggleTheme();;
     },[setIsDarkMode])
